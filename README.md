28306d866f0d702265449911639db440646a2b75
tiagostutz <tiagostutz@bb.com.br>
2018-09-22 17:26:37 -0300

    Create, GetAll, Get, Delete successfully working. ;)

--- README.md
+++ README.md
@@ -1,6 +1,57 @@
 # schelly-postgres
 
+# Usage
 
+docker-compose .yml
+
+```yml
+version: '3.5'
+
+services:
+
+  db:
+    image: postgres
+    environment:
+      - POSTGRES_DB=schelly
+      - POSTGRES_PASSWORD=postgres
+
+  schelly-postgres-repo:
+    image: tiagostutz/schelly-postgres
+    build: .
+    ports:
+      - 7070:7070
+    environment:
+      - LOG_LEVEL=debug
+      - BACKUP_FILE_PATH=/var/backups
+      - DATABASE_NAME=schelly
+      - DATABASE_CONNECTION_HOST=db
+      - DATABASE_CONNECTION_PORT=5432
+      - DATABASE_AUTH_USERNAME=postgres
+      - DATABASE_AUTH_PASSWORD=postgres
+
+networks:
+  default:
+    name: schelly-postgres-net
+```
+
+```shell
+#create a new backup
+curl -X POST http://localhost:7070/backups
+
+#list existing backups
+curl -X GET http://localhost:7070/backups
+
+#get info about an specific backup
+curl _X GET http://localhost:7070/backups/abc123
+
+#remove existing backup
+curl -X DELETE localhost:7070/backups/abc123
+
+```
+
+# REST Endpoints
+
+As in https://github.com/flaviostutz/schelly#webhook-spec
 
 `pg_dump` parameters that can be set:
 
@@ -29,13 +80,13 @@ Schelly postgres repo custom options:
 ```
 General options:
   --format=c|d|t|p         output file format (custom, directory, tar,
-                               plain text (default))                               -> value used: tar
-  --jobs=NUM               use this many parallel jobs to dump                 -> value used: 1
-  --verbose                verbose mode                                        -> value used: --verbose  
-  --compress=0-9           compression level for compressed formats            -> value used: 9
-  --column-inserts             dump data as INSERT commands with column names      -> value used: --column-inserts
-  --inserts                    dump data as INSERT commands, rather than COPY      -> value used: --inserts  
-  --quote-all-identifiers      quote all identifiers, even if not key words        -> value used: --quote-all-identifiers
+                               plain text (default))                                 -> value used: p
+  --jobs=NUM               use this many parallel jobs to dump                      -> value used: 1
+  --verbose                verbose mode                                             -> value used: --verbose  
+  --compress=0-9           compression level for compressed formats                 -> value used: 9
+  --column-inserts             dump data as INSERT commands with column names       -> value used: --column-inserts
+  --inserts                    dump data as INSERT commands, rather than COPY       -> value used: --inserts  
+  --quote-all-identifiers      quote all identifiers, even if not key words         -> value used: --quote-all-identifiers
 
 Options controlling the output content:
   --clean                  clean (drop) database objects before recreating     -> value used: --clean  
--- schelly-postgres/postgresrepo.go
+++ schelly-postgres/postgresrepo.go
@@ -4,9 +4,7 @@ import (
 	"flag"
 	"fmt"
 	"io/ioutil"
-	"log"
 	"os"
-	"os/user"
 	"strconv"
 	"strings"
 	"time"
@@ -14,6 +12,8 @@ import (
 	"github.com/flaviostutz/schelly-webhook/schellyhook"
 	"go.uber.org/zap"
 )
+
+var dataStringSeparator string
 
 // backups directory where the backup files will be placed
 var backupsDir *string
@@ -48,6 +48,8 @@ func (sb PostgresBackuper) Init() error {
 	defer logger.Sync() // flushes buffer, if any
 	sugar := logger.Sugar()
 
+	dataStringSeparator = "---"
+
 	info, err := schellyhook.ExecShell("pg_dump --version")
 	if err != nil {
 		sugar.Errorf("Couldn't retrieve pg_dump version. err=%s", err)
@@ -75,27 +77,36 @@ func (sb PostgresBackuper) Init() error {
 	if *password == "" {
 		return fmt.Errorf("`password` (--password) arg must be set")
 	}
-
-	pgPassStringBytes := []byte(*host + ":" + strconv.Itoa(*port) + ":" + *username + ":" + *dbname + ":" + *password)
-
-	usr, err := user.Current()
+	basicDir := "/var/backups"
+	err = mkDirs(basicDir)
 	if err != nil {
-		log.Fatal(err)
+		return fmt.Errorf("Error creating basic workdir /var/backups")
 	}
-	err = ioutil.WriteFile(usr.HomeDir+".pgpass", pgPassStringBytes, 0644)
+
+	// About .pgpass file
+	// https://www.postgresql.org/docs/9.3/static/libpq-pgpass.html
+	pgPassFilePath := basicDir + "/.pgpass"
+	os.Setenv("PGPASSFILE", pgPassFilePath)
+	pgPassStringBytes := []byte("*:*:*:*:" + *password)
+	err = ioutil.WriteFile(pgPassFilePath, pgPassStringBytes, 0600)
 	if err != nil {
 		sugar.Errorf("Error writing .pgpass file. err: %s", err)
 		return err
 	}
 
-	pgPassFile, err := ioutil.ReadFile(usr.HomeDir + ".pgpass")
+	pgPassFile, err := ioutil.ReadFile(pgPassFilePath)
 	if err != nil {
 		sugar.Errorf("Error reading .pgpass file. err: %s", err)
 		return err
 	}
-	sugar.Debugf("~/.pgpass file created. Contents: %s", pgPassFile)
+	sugar.Debugf(pgPassFilePath+" file created. Contents: %s", pgPassFile)
 
-	sugar.Infof("Postgres Repository ready to work. Version: ", info)
+	err = mkDirs(*backupsDir)
+	if err != nil {
+		return fmt.Errorf("Error creating backups `base-dir`. error: %s", err)
+	}
+
+	sugar.Infof("Postgres Repository ready to work. Version: %s", info)
 
 	return nil
 }
@@ -144,7 +155,7 @@ func (sb PostgresBackuper) CreateNewBackup(apiID string, timeout time.Duration, 
 	sugar.Infof("Running Postgres pg_dump backup")
 
 	pgDumpID := time.Now().Format("20060102150405")
-	fileString := "--file=" + resolveFileName(pgDumpID, apiID)
+	fileString := "--file=" + resolveFilePath(apiID, pgDumpID)
 
 	dataOnlyString := ""
 	if *dataOnly == true {
@@ -163,7 +174,9 @@ func (sb PostgresBackuper) CreateNewBackup(apiID string, timeout time.Duration, 
 		backupFormat = "p"
 	}
 
-	out, err := schellyhook.ExecShellTimeout("pg_dump --verbose --format="+backupFormat+" --jobs=1 --compress=9 --column-inserts --inserts --quote-all-identifiers --clean --create "+fileString+" "+dataOnlyString+" "+schemaOnlyString+" "+encodingString, timeout, shellContext)
+	pgDumpCommand := "pg_dump --username=" + *username + " --dbname=" + *dbname + " --host=" + *host + " --port=" + strconv.Itoa(*port) + " --verbose --format=" + backupFormat + " --jobs=1 --compress=9 --column-inserts --inserts --quote-all-identifiers --clean --create " + fileString + " " + dataOnlyString + " " + schemaOnlyString + " " + encodingString
+	sugar.Debugf("Executing pg_dump command: %s", pgDumpCommand)
+	out, err := schellyhook.ExecShellTimeout(pgDumpCommand, timeout, shellContext)
 
 	if err != nil {
 		status := (*shellContext).CmdRef.Status()
@@ -185,10 +198,6 @@ func (sb PostgresBackuper) CreateNewBackup(apiID string, timeout time.Duration, 
 	return nil
 }
 
-func resolveFileName(apiID string, pgDumpID string) string {
-	return *backupsDir + "/" + *fileName + "--" + pgDumpID + "---" + apiID
-}
-
 //GetAllBackups returns all backups from underlaying backuper. optional for Schelly
 func (sb PostgresBackuper) GetAllBackups() ([]schellyhook.SchellyResponse, error) {
 	logger, _ := zap.NewDevelopment()
@@ -196,32 +205,32 @@ func (sb PostgresBackuper) GetAllBackups() ([]schellyhook.SchellyResponse, error
 	sugar := logger.Sugar()
 
 	sugar.Debugf("GetAllBackups")
-	result, err := schellyhook.ExecShell("ls -l" + *backupsDir)
+	files, err := ioutil.ReadDir(*backupsDir)
 	if err != nil {
 		return nil, err
 	}
 
 	backups := make([]schellyhook.SchellyResponse, 0)
-	lines := strings.Split(result, "\n")
-	for i, line := range lines {
-		cols := strings.Split(line, " ")
-		if i == 0 || len(cols) < 8 {
-			continue
-		}
+	for _, fileName := range files {
 
-		dataID := strings.Split(cols[8], "--")[1]
-		sizeMB, err := strconv.ParseFloat(cols[4], 64)
+		id := strings.Split(fileName.Name(), dataStringSeparator)[1]
+		dataID := strings.Split(fileName.Name(), dataStringSeparator)[2]
+		sizeMB := fileName.Size()
+
+		backupFilePath := *backupsDir + "/" + fileName.Name()
+		_, err = os.Open(backupFilePath)
 		if err != nil {
 			return nil, err
 		}
-		sizeMB = sizeMB / 1000000.0
+		sugar.Debugf("Found and opened backup file: %s", backupFilePath)
 		status := "available"
 
 		sr := schellyhook.SchellyResponse{
+			ID:      id,
 			DataID:  dataID,
 			Status:  status,
-			Message: line,
-			SizeMB:  sizeMB,
+			Message: backupFilePath,
+			SizeMB:  float64(sizeMB),
 		}
 		backups = append(backups, sr)
 	}
@@ -240,9 +249,13 @@ func (sb PostgresBackuper) GetBackup(apiID string) (*schellyhook.SchellyResponse
 	pgDumpID, err0 := getDataID(apiID)
 	if err0 != nil {
 		sugar.Debugf("pgDumpID not found for apiId %s. err=%s", apiID, err0)
-		return nil, nil
+		return nil, err0
+	}
+	if pgDumpID == "" {
+		return nil, fmt.Errorf("pgDumpID not found for apiId %s", apiID)
 	}
 
+	sugar.Debugf("Found pgDumpID=" + pgDumpID + " for apiID: " + apiID + ". Finding Backup file...")
 	res, err := findBackup(apiID, pgDumpID)
 	if err != nil {
 		return nil, nil
@@ -272,16 +285,11 @@ func (sb PostgresBackuper) DeleteBackup(apiID string) error {
 	}
 
 	sugar.Debugf("Backup apiID=%s pgDumpID=%s found. Proceeding to deletion", apiID, pgDumpID)
-	result, err := schellyhook.ExecShell("rm " + resolveFileName(apiID, pgDumpID))
-	if err != nil {
-		if strings.Contains(err.Error(), "100") {
-			return fmt.Errorf("Cannot delete this backup because it is too young. Configure $PROTECT_YOUNG_BACKUP_DAYS if needed. err=%s", err)
-		} else {
-			return err
-		}
-	}
-	sugar.Debugf("result: %s", result)
 
+	err1 := os.Remove(resolveFilePath(apiID, pgDumpID))
+	if err1 != nil {
+		return err1
+	}
 	sugar.Debugf("Delete apiID %s pgDumpID %s successful", apiID, pgDumpID)
 	return nil
 }
@@ -291,28 +299,27 @@ func findBackup(apiID string, pgDumpID string) (*schellyhook.SchellyResponse, er
 	defer logger.Sync() // flushes buffer, if any
 	sugar := logger.Sugar()
 
-	result, err := schellyhook.ExecShell("ls -l " + resolveFileName(apiID, pgDumpID))
+	backupFilePath := resolveFilePath(apiID, pgDumpID)
+	result, err := os.Open(backupFilePath)
+	if err != nil {
+		sugar.Errorf("File " + backupFilePath + " no found")
+		return nil, err
+	}
+	file, err := result.Stat()
 	if err != nil {
 		return nil, err
 	}
 
-	cols := strings.Split(result, " ")
-	if len(cols) < 6 {
-		return nil, fmt.Errorf("pgDumpID file not found or corrupted")
-	}
-	sugar.Debugf("pgDumpID found. Details: %s", cols)
+	sugar.Debugf("pgDumpID found. Details: %s", file)
 
-	size, err := strconv.ParseFloat(cols[4], 64)
-	if err != nil {
-		return nil, err
-	}
 	status := "available"
 
 	return &schellyhook.SchellyResponse{
-		ID:     apiID,
-		DataID: pgDumpID,
-		Status: status,
-		SizeMB: size / 1000000.0,
+		ID:      apiID,
+		DataID:  pgDumpID,
+		Status:  status,
+		Message: backupFilePath,
+		SizeMB:  float64(file.Size()),
 	}, nil
 }
 
@@ -321,26 +328,30 @@ func getDataID(apiID string) (string, error) {
 	defer logger.Sync() // flushes buffer, if any
 	sugar := logger.Sugar()
 
-	result, err := schellyhook.ExecShell("ls -m " + *backupsDir)
-	if err == nil {
+	sugar.Debugf("Searching dataID (pgDumpID) for apiID: %s", apiID)
+	files, err := ioutil.ReadDir(*backupsDir)
+	if err != nil {
 		return "", err
 	}
-	files := strings.Split(result, ",")
 	for _, file := range files {
-		if strings.Contains(file, apiID) {
-			if _, err := os.Stat(file); err == nil {
-				sugar.Debugf("Found api id reference for %s", apiID)
-				_, err0 := ioutil.ReadFile(file)
+		sugar.Debugf("Backup File <Loop>: %s", file.Name())
+		if strings.Contains(file.Name(), apiID) {
+			if _, err := os.Stat(*backupsDir + "/" + file.Name()); err == nil {
+				sugar.Debugf("Found file for apiID reference: %s", apiID)
+				_, err0 := ioutil.ReadFile(*backupsDir + "/" + file.Name())
 				if err0 != nil {
 					return "", err0
 				}
-				pgDumpID := strings.Split(file, "--")[1]
+				pgDumpID := strings.Split(file.Name(), dataStringSeparator)[2]
 				sugar.Debugf("apiID %s <-> pgDumpID %s", apiID, pgDumpID)
 				return pgDumpID, nil
 			}
 		}
 	}
 	return "", fmt.Errorf("pgDumpID for %s not found", apiID)
+}
+func resolveFilePath(apiID string, pgDumpID string) string {
+	return *backupsDir + "/" + *fileName + dataStringSeparator + apiID + dataStringSeparator + pgDumpID
 }
 
 func saveDataID(apiID string, pgDumpID string) error {
@@ -349,5 +360,12 @@ func saveDataID(apiID string, pgDumpID string) error {
 	sugar := logger.Sugar()
 
 	sugar.Debugf("IDs already saved apiID %s <-> pgDumpID %s", apiID, pgDumpID)
+	return nil
+}
+
+func mkDirs(path string) error {
+	if _, err := os.Stat(path); os.IsNotExist(err) {
+		return os.MkdirAll(path, os.ModePerm)
+	}
 	return nil
 }
